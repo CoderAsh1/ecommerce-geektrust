b94c2c1c62cc01c76ad738a07ce8c85f1fbc54fc
coderAsh1 <ashutoshsasmal1@gmail.com>
2022-11-18 21:16:37 +0530

    cart- total-update done

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import "./App.scss";
 import cartIcon from "./assets/cart-shopping-solid.gif";
 import { ecomContext } from "./context/ContextApi";
 import Products from "./pages/products/Products";
+import Cart from "./pages/cart/Cart";
+import { Link, Route, Routes } from "react-router-dom";
 function App() {
   const {
-    state: { products, cart },
+    state: { cart },
   } = useContext(ecomContext);
 
   return (
@@ -13,12 +15,15 @@ function App() {
       <nav>
         <h3>Teerex Store</h3>
         <span>Products</span>
-        <div className="cart">
+        <Link className="cartIcon" to="/cart">
           <img src={cartIcon} alt="cartIcon" height={"40px"} />
           <span>{cart.length}</span>
-        </div>
+        </Link>
       </nav>
-      <Products />
+      <Routes>
+        <Route exact path="/" element={<Products />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useContext } from "react";
+import "./cart.scss";
+import { ecomContext } from "../../context/ContextApi";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const {
+    state: { cart },
+    dispatch,
+  } = useContext(ecomContext);
+
+  console.log(cart);
+  return (
+    <div className="cart">
+      <h4>Shopping Cart</h4>
+      <div className="items">
+        {cart.map((item) => (
+          <div className="item" key={item.id}>
+            <img src={item.imageURL} alt="imageURL" width={"50px"} />
+            <div className="title-price">
+              <p>{item.name}</p>
+              <p>Rs.{item.price}</p>
+            </div>
+            <div className="count">
+              <button>-</button>
+              <h4> {item.qty}</h4>
+              <button>+</button>
+            </div>
+            <button>Delete</button>
+          </div>
+        ))}
+        <hr />
+        <h4 className="total">
+          Total amount : {cart.reduce((acc, cur) => acc + cur.price, 0)}
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
